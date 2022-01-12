71de1d90103f6be9940fc2daaa85c7b50502a8ad
andre.ribeiro <ribeirolimand@gmail.com>
2022-01-12 08:17:01 -0300

    auth test

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 # imports from third-party libraries
 from typing import Optional
+
+from sqlalchemy.util.langhelpers import dependencies
 from fastapi import FastAPI, Depends, HTTPException, status, Form
 from fastapi.security import APIKeyHeader
 
@@ -13,6 +15,7 @@ from sqlalchemy.orm import Session
 
 # required imports from utils package
 from routers import organizations, tickets, time_appointments, webhooks, users
+from auth import auth
 
 ################## constants ####################
 DESCRIPTION = """
@@ -31,16 +34,15 @@ def startup_event():
     # logger.info("Models created/updated sucessfuly")
 
 
-# @app.get("/test")
-# def get_test_endpoint(token: str=None,
-#                         db: Session=Depends(get_db)):
-#     if not token:
-#         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+@app.get("/test", dependencies=[Depends(auth.authorization)])
+def get_test_endpoint(db: Session=Depends(get_db)):
+    # if not token:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
 
-#     acess_token = crud.get_user_by_token(db, token)
-#     if not acess_token:
-#         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-#     return {"hello": "world"}
+    # acess_token = crud.get_user_by_token(db, token)
+    # if not acess_token:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    return {"hello": "world"}
 
 
 # @app.get("/")
