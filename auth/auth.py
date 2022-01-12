bcae1288af78166195f5a5ab1867d92d7000e461
andre.ribeiro <ribeirolimand@gmail.com>
2022-01-12 20:08:07 -0300

    adjust auth

--- auth/auth.py
+++ auth/auth.py
@@ -6,7 +6,7 @@ from utils.handlers import get_db
 
 def authorization(db, token):
     if not token:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     acess_token = crud.get_user_by_token(db, token)
     if not acess_token:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
@@ -14,7 +14,7 @@ def authorization(db, token):
 
 def admin_authorization(db, token):
     if not token:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     acess_token = crud.get_user_by_token(db, token)
     if not acess_token and acess_token.isadmin:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
@@ -23,7 +23,7 @@ def admin_authorization(db, token):
 def api_token(token: str=Depends(APIKeyQuery(name="Token")), 
                 db: Session=Depends(get_db)):
     if not token:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     acess_token = crud.get_user_by_token(db, token)
     if not acess_token:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
