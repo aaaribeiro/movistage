6de7361424a8178a2b9bb5247f9fbbfeb2516a81
andre.ribeiro <ribeirolimand@gmail.com>
2022-02-02 18:04:02 -0300

    beta 1.2

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -1,9 +1,7 @@
 # built-in libraries
-from datetime import datetime
 
 # imports from third-party libraries
-from typing import List
-from fastapi import Depends, APIRouter, HTTPException, Request, status
+from fastapi import Depends, APIRouter, Request, status
 from sqlalchemy.orm import Session
 
 # required imports from package models
@@ -30,7 +28,7 @@ router = APIRouter()
     "/listener-ticket",
     tags=TAGS,
     status_code=status.HTTP_201_CREATED, 
-    response_model=schemas.WebhookLog,
+    # response_model=schemas.WebhookLog,
     # dependencies=[Depends(auth.api_token)],
 )
 async def crud_ticket(request: Request, db: Session=Depends(get_db)):
@@ -51,7 +49,7 @@ async def crud_ticket(request: Request, db: Session=Depends(get_db)):
     "/listener-appointment",
     tags=TAGS,
     status_code=status.HTTP_201_CREATED, 
-    response_model=schemas.WebhookLog,
+    # response_model=schemas.WebhookLog,
     # dependencies=[Depends(auth.api_token)],
 )
 async def crud_appointment(request: Request, db: Session=Depends(get_db)):
@@ -65,105 +63,3 @@ async def crud_appointment(request: Request, db: Session=Depends(get_db)):
                                                     appointment.time_appointment_id)
         if not dbappointment:
             crud.create_time_appointment(db, appointment)
-
-
-# @router.post(
-#     "/hook/update/ticket",
-#     tags=TAGS,
-#     status_code=status.HTTP_201_CREATED, 
-#     # response_model=schemas.WebhookLog,
-#     dependencies=[Depends(auth.api_token)],
-# )
-# async def create_hook_for_update_ticket(request: Request, token: str = None,
-#                     db: Session=Depends(get_db)):
-#     """
-#     Write something
-#     """
-#     response = await request.json()
-#     webhook = schemas.WebhookLog(
-#         ticket_id = response["Id"],
-#         change = "UPDATE TICKET",
-#         trigger_date = datetime.now(),
-#         was_read = False
-#     )
-#     _crud.create_hook(db, webhook)
-#     # webhook = crud.create_hook(db, webhook)
-#     # return webhook
-
-
-# @router.post(
-#     "/hook/new/appointment",
-#     tags=TAGS,
-#     status_code=status.HTTP_201_CREATED, 
-#     # response_model=schemas.WebhookLog,
-#     dependencies=[Depends(auth.api_token)],
-# )
-# async def create_hook_for_appointment(request: Request, token: str = None,
-#                     db: Session=Depends(get_db)):
-#     """
-#     Write something
-#     """
-#     response = await request.json()
-#     webhook = schemas.WebhookLog(
-#         ticket_id = response["Id"],
-#         change = "NEW APPOINTMENT",
-#         trigger_date = datetime.now(),
-#         was_read = False
-#     )
-#     _crud.create_hook(db, webhook)
-#     # webhook = crud.create_hook(db, webhook)
-#     # return webhook
-
-
-# @router.get(
-#     "/hooks",
-#     tags=TAGS,
-#     response_model=List[schemas.WebhookLog],
-#     dependencies=[Depends(auth.api_token)],
-# )
-# async def read_hooks(token: str = None, read: bool = None,
-#                         skip: int = 0, limit: int = 1000,
-#                         db:Session=Depends(get_db)):
-#     """
-#     Returns a hook list by ticket id
-#     """
-#     hooks = _crud.get_hook_logs(db, read=read, skip=skip, limit=limit)
-#     if not hooks:
-#         raise HTTPException(status_code=400, detail="All hooks were read")
-#     return hooks
-
-
-# @router.get(
-#     "/hook/{ticket_id}",
-#     tags=TAGS,
-#     response_model=List[schemas.WebhookLog],
-#     dependencies=[Depends(auth.api_token)],
-# )
-# async def read_hooks_by_ticket_id(ticket_id: int, token: str = None,
-#                         skip: int = 0, limit: int = 100,
-#                         db:Session=Depends(get_db)):
-#     """
-#     Returns a hook list by ticket id
-#     """
-#     hooks = _crud.get_hooks_by_ticket_id(db, ticket_id=ticket_id, skip=skip,
-#         limit=limit)
-#     return hooks
-
-
-# @router.patch(
-#     "/hook/{hook_id}",
-#     tags=TAGS,
-#     status_code=status.HTTP_204_NO_CONTENT, 
-#     # response_model=schemas.WebhookLog,
-#     dependencies=[Depends(auth.api_token)],
-# )
-# async def update_hook(hook_id: str, hook: schemas.WebhookLog, token:str=None,
-#                         db: Session = Depends(get_db)):
-#     """
-#     Partial update for webhook logs
-#     """
-#     stored_hook = _crud.get_hook_by_id(db, id=hook_id)
-#     if not stored_hook:
-#         raise HTTPException(status_code=404, detail="Hook Not Found")
-    
-#     _crud.partial_update_hook(db, id=hook_id, hook=hook,)
