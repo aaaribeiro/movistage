db2c9b7e0aefcc15faee0c7dc6581e9c80516037
andre.ribeiro <ribeirolimand@gmail.com>
2022-02-07 23:34:33 -0300

    beta 1.2

--- models/models.py
+++ models/models.py
@@ -29,7 +29,7 @@ class Tickets(Base):
     sla_solution_date = Column(DateTime)
     sla_first_response = Column(DateTime)
     # relationships
-    time_appointments = relationship("TimeAppointments")
+    appointments = relationship("TimeAppointments")
     organization =  relationship("Organizations", back_populates="tickets")
     agent = relationship("Agents", back_populates="tickets")
 
