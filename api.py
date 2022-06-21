469b7cb79fa5e625df7ca01eaec1d52b76576c2e
andre.ribeiro <ribeirolimand@gmail.com>
2022-06-21 11:18:37 -0300

    retake the project

--- api.py
+++ api.py
@@ -5,8 +5,8 @@ from fastapi import FastAPI #, Depends, HTTPException, status ,Form
 # from fastapi.security import APIKeyHeader
 
 # required imports from models package
-from models import models
-from models.database import engine
+# from models import models
+# from models.database import engine
 # from models import crud
 
 # required imports from package utils 
@@ -14,13 +14,13 @@ from models.database import engine
 # from sqlalchemy.orm import Session
 
 # required imports from utils package
-from routers import agents, appointments, listeners, organizations, tickets, users
+# from routers import agents, appointments, listeners, organizations, tickets, users
 # from auth import auth
 
 ################## constants ####################
 DESCRIPTION = """
 """
-PREFIX = "/stage/movidesk/v1"
+# PREFIX = "/stage/movidesk/v1"
 #################################################
 
 app = FastAPI(
@@ -38,9 +38,9 @@ app = FastAPI(
 #         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
 
 
-@app.on_event("startup")
-def startup_event():
-    models.Base.metadata.create_all(bind=engine)
+# @app.on_event("startup")
+# def startup_event():
+#     models.Base.metadata.create_all(bind=engine)
 
 
 # @app.get("/test", dependencies=[Depends(auth.api_token)])
@@ -59,9 +59,9 @@ def startup_event():
 def get_root():
     return {"message": "welcome to Stage"}
 
-app.include_router(tickets.router, prefix=PREFIX)
-app.include_router(organizations.router, prefix=PREFIX)
-app.include_router(agents.router, prefix=PREFIX)
-app.include_router(appointments.router, prefix=PREFIX)
-app.include_router(listeners.router, prefix=PREFIX)
+# app.include_router(tickets.router, prefix=PREFIX)
+# app.include_router(organizations.router, prefix=PREFIX)
+# app.include_router(agents.router, prefix=PREFIX)
+# app.include_router(appointments.router, prefix=PREFIX)
+# app.include_router(listeners.router, prefix=PREFIX)
 # app.include_router(users.router, prefix=PREFIX)
